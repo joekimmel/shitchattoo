f99a980fd9fa47ae8e72dc30a42655f87a181b96
Joe <jmkimmel@gmail.com>
2016-10-28 23:08:25 +0000

    buffer the last 20 messages so clients who join can see what was just said

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, session, request
-from flask_socketio import SocketIO, emit, join_room, leave_room, rooms, disconnect, close_room
+from collections import deque
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Burton'
 socketio = SocketIO(app, async_mode=None)
 
+messages = deque()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', messages=messages)
 
 @socketio.on('trd_connect_event', namespace='/trd')
 def connect_event(evt):
@@ -17,10 +20,17 @@ def connect_event(evt):
 
 @socketio.on('trd_broadcast_event', namespace='/trd')
 def broadcast_message(message):
-    print("broadcast recieved from: " + message['sender'] + "message: "+message['msg'])
+    outbound = {
+        'msg': message['msg'],
+        'sender': message['sender']}
+
+    #keep a buffer of the last 20 messages for new clients who are joining...
+    messages.append(outbound)
+    while(len(messages) > 20 ):
+        messages.popleft()
+
     emit('chat_message',
-         {'msg': message['msg'],
-           'sender': message['sender']},
+         outbound,
          broadcast=True)
 
 
